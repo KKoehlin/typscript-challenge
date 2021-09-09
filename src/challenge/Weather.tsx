e54753e73f7bc96401711e3279f47775caa111a5
Kaylee Koehlinger <kkoehlin@gmail.com>
2021-09-09 17:30:26 -0400

    updated weather

--- src/challenge/Weather.tsx
+++ src/challenge/Weather.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import GeoLocation from './GeoLocation'
 
 
 
-const Weather = () => {
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.long}&appid=8f0eb5852d755da347226e497f33dbd5`;
+function Weather() {
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=8f0eb5852d755da347226e497f33dbd5`
 
-    async fetch(url)
+    useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json)
+    })
+    }, [url])
+
 
 
 
